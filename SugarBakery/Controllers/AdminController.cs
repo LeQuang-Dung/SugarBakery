9696c4a3c17a4bff3ee6e6d2aa895030a8f30bc0
Bob <dinhtruong.tech@gmail.com>
2022-06-22 17:16:49 +0700

    upload again

---
+++ SugarBakery/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SugarBakery.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace SugarBakery.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbSugarBakeryDataContext data = new dbSugarBakeryDataContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if (Session["TKAdmin"] == null)
+            {
+                return RedirectToAction("Index", "Fashion");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            string user = collection["form-username"];
+            string pass = collection["form-password"];
+
+            tbAdmin ad = data.tbAdmins.SingleOrDefault(a => a.UserAdmin == user && a.PassAdmin == pass );
+            if (ad == null)
+            {
+                ViewBag.ThongBaoAdmin = "Tài Khoản Hoặc Mật Khẩu Sai";
+                return this.Login();
+            }
+            Session["TKAdmin"] = ad;
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
---
+++ SugarBakery/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SugarBakery.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace SugarBakery.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbSugarBakeryDataContext data = new dbSugarBakeryDataContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult TinTuc()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LienHe(FormCollection collection)
+        {
+            if (Session["Taikhoan"] == null || Session["Taikhoan"].ToString() == "")
+            {
+                return RedirectToAction("Login", "NguoiDung");
+            }
+            else
+            {
+                tbPhanHoiKH ht = new tbPhanHoiKH();
+                tbKhachHang kh = (tbKhachHang)Session["Taikhoan"];
+                ht.MaKH = kh.MaKH;
+                ht.HoTen = kh.TenKH;
+                ht.Email = kh.Email;
+                string lydo = collection["LyDo"];
+                ht.LyDo = lydo;
+                if (lydo == null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    data.tbPhanHoiKHs.InsertOnSubmit(ht);
+                    data.SubmitChanges();
+                    return RedirectToAction("Contact", "Home");
+                }
+            }
+        }
+    }
+}
